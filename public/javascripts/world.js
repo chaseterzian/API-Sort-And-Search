dbbcf93a369a547f9aea8a3a8ff8aa57b8503bb9
ChaseTerzian <chaseterzian@gmail.com>
2019-11-22 21:32:01 -0700

    basic functionailty for when user drills down to last remaining country line

--- public/javascripts/world.js
+++ public/javascripts/world.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 				td3.innerHTML = (data[i].region);
 				tr.appendChild(td3);
 
-				document.getElementById('api-table').appendChild(tr);
+				document.getElementById('api-tbody').appendChild(tr);
 			}
 
 		});
@@ -37,52 +37,71 @@ $(document).ready(function() {
 
 		query = document.getElementById('search-field').value.toLowerCase();
 
-		$('tr td').hide();
+		$('#api-table tr td').hide();
+		$('#final-line-wrap').hide();
 
 		for (var x = 0; x < dataApi.length; x++) {
 
 			nameCol = dataApi[x].name.toLowerCase();
 			popCol = dataApi[x].population.toString();
 			regCol = dataApi[x].region.toLowerCase();
-			// lt = false;
-			// gt = false;
 
 			if (query.indexOf("<") > -1 || query.indexOf("< ") > -1) {
-				// lt = true;
 				if (parseInt(popCol) <= parseInt(query.substring(1))) {
-					$('tr').eq(x+1).find('td').eq(0).show();
-					$('tr').eq(x+1).find('td').eq(1).show();
-					$('tr').eq(x+1).find('td').eq(2).show();
+					$('#api-table tr').eq(x+1).find('td').eq(0).addClass(x.toString()).show();
+					$('#api-table tr').eq(x+1).find('td').eq(1).addClass(x.toString()).show();
+					$('#api-table tr').eq(x+1).find('td').eq(2).addClass(x.toString()).show();
 				}
 			}
 
 			if (query.indexOf(">") > -1 || query.indexOf(">") > -1) {
-				// gt = true;
 				if (parseInt(popCol) >= parseInt(query.substring(1))) {
-					$('tr').eq(x+1).find('td').eq(0).show();
-					$('tr').eq(x+1).find('td').eq(1).show();
-					$('tr').eq(x+1).find('td').eq(2).show();
+					$('#api-table tr').eq(x+1).find('td').eq(0).addClass(x.toString()).show();
+					$('#api-table tr').eq(x+1).find('td').eq(1).addClass(x.toString()).show();
+					$('#api-table tr').eq(x+1).find('td').eq(2).addClass(x.toString()).show();
 				}
 			}
 
 			if (popCol.indexOf(query) > - 1) {
-				$('tr').eq(x+1).find('td').eq(0).show();
-				$('tr').eq(x+1).find('td').eq(1).show();
-				$('tr').eq(x+1).find('td').eq(2).show();
+				$('#api-table tr').eq(x+1).find('td').eq(0).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(1).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(2).addClass(x.toString()).show();
 			}
 
 			if (nameCol.indexOf(query) > - 1) {
-				$('tr').eq(x+1).find('td').eq(0).show();
-				$('tr').eq(x+1).find('td').eq(1).show();
-				$('tr').eq(x+1).find('td').eq(2).show();
+				$('#api-table tr').eq(x+1).find('td').eq(0).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(1).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(2).addClass(x.toString()).show();
 			}
 
 			if (regCol.indexOf(query) > - 1) {
-				$('tr').eq(x+1).find('td').eq(0).show();
-				$('tr').eq(x+1).find('td').eq(1).show();
-				$('tr').eq(x+1).find('td').eq(2).show();
+				$('#api-table tr').eq(x+1).find('td').eq(0).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(1).addClass(x.toString()).show();
+				$('#api-table tr').eq(x+1).find('td').eq(2).addClass(x.toString()).show();
 			}
 
+		}
+
+		if ($('#api-table td:visible').length === 3) {
+			$('#final-line-wrap').show();
+
+			dataApiIndex = $('#api-table td:visible').attr('class');
+			
+			$('#api-table tr th').hide();
+			$('#api-table td:visible').hide();
+
+			document.getElementById('final-line-img').src = dataApi[dataApiIndex].flag;
+			document.getElementById('final-line-name').innerHTML = dataApi[dataApiIndex].name;
+			document.getElementById('final-line-population').innerHTML = dataApi[dataApiIndex].region;
+			document.getElementById('final-line-region').innerHTML = dataApi[dataApiIndex].population;
+			document.getElementById('final-line-country-code').innerHTML = dataApi[dataApiIndex].alpha3Code;
+			// document.getElementById('final-line-bordering-countries').innerHTML = dataApi[dataApiIndex].borders; 
+			document.getElementById('final-line-capital').innerHTML = dataApi[dataApiIndex].capital; 
+			// document.getElementById('final-line-currency-code').innerHTML = dataApi[dataApiIndex].currencies.code; 
+			// document.getElementById('final-line-currency-name').innerHTML = dataApi[dataApiIndex].currencies.name; 
+			// document.getElementById('final-line-currency-symbol').innerHTML = dataApi[dataApiIndex].currencies.symbol; 
+			// document.getElementById('final-line-language').innerHTML = dataApi[dataApiIndex].languages.name; 
+			// document.getElementById('final-line-tld').innerHTML = dataApi[dataApiIndex].topLevelDomain; 
 		}
 
 	});
