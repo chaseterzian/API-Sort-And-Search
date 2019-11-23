cec59e5f31fd8db3a668a9180df0e10eba06b343
ChaseTerzian <chaseterzian@gmail.com>
2019-11-23 00:57:17 -0700

    fix copy, add image to page, adjust spacing

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( express.static( "public" ) ); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
